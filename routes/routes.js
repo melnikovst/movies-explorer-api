2c64282e15d2f35ef90bd1924a43599e22df13fc
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-12-05 19:31:18 +0300

    das

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,7 @@ const route = require('express').Router();
 
 const user = require('./user');
 const movies = require('./movies');
-const { auth } = require('../middlewares/auth');
-const { validateAuth } = require('../middlewares/joi');
-const { postProfile, login } = require('../controllers/users');
 
-route.post('/signup', validateAuth, postProfile);
-route.post('/signin', validateAuth, login);
-route.use(auth);
 route.use(user);
 route.use(movies);
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,14 @@
 const userRouter = require('express').Router();
 
-const { me, signout, updateUser } = require('../controllers/users');
-const { validateUpdateProfile } = require('../middlewares/joi');
+const { auth } = require('../middlewares/auth');
+const {
+  postProfile, me, login, signout, updateUser,
+} = require('../controllers/users');
+const { validateAuth, validateUpdateProfile } = require('../middlewares/joi');
 
+userRouter.post('/signup', validateAuth, postProfile);
+userRouter.post('/signin', validateAuth, login);
+userRouter.use(auth);
 userRouter.get('/users/me', me);
 userRouter.get('/signout', signout);
 userRouter.patch('/users/me', validateUpdateProfile, updateUser);

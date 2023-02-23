834f7392d5950fa4ad09796b33d70709b1764b37
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2023-02-23 17:31:31 +0300

    cors

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,31 @@
-const crypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const crypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const User = require('../models/user');
-const BadRequest = require('../errors/BadRequest');
-const Conflict = require('../errors/Conflict');
+const User = require("../models/user");
+const BadRequest = require("../errors/BadRequest");
+const Conflict = require("../errors/Conflict");
 const {
-  BAD_REQUEST_VALIDATION_ERROR, CONFLICT_ERROR, WRONG_DATA_RESPONSE, SUCCESS_LOGIN,
-} = require('../utils/constants');
-const Forbidden = require('../errors/Forbidden');
+  BAD_REQUEST_VALIDATION_ERROR,
+  CONFLICT_ERROR,
+  WRONG_DATA_RESPONSE,
+  SUCCESS_LOGIN,
+} = require("../utils/constants");
+const Forbidden = require("../errors/Forbidden");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.postProfile = async (req, res, next) => {
-  const {
-    email, password, name,
-  } = req.body;
+  const { email, password, name } = req.body;
   try {
     const hashedPassword = await crypt.hash(password, 5);
     const response = await User.create({
-      email, name, password: hashedPassword,
+      email,
+      name,
+      password: hashedPassword,
     });
     res.send(response);
   } catch (error) {
-    if (error.name === 'ValidationError') {
+    if (error.name === "ValidationError") {
       return next(new BadRequest(BAD_REQUEST_VALIDATION_ERROR));
     }
     if (error.code === 11000) {
@@ -38,7 +41,10 @@ module.exports.updateUser = async (req, res, next) => {
     const user = { name: req.body.name, email: req.body.email };
     const owner = req.user._id;
     const dbUser = await User.findOne({ email });
-    const val = await User.findByIdAndUpdate(owner, user, { new: true, runValidators: true });
+    const val = await User.findByIdAndUpdate(owner, user, {
+      new: true,
+      runValidators: true,
+    });
     if (dbUser) {
       if (dbUser.email === val.email) {
         return next(new Conflict(CONFLICT_ERROR));
@@ -46,7 +52,7 @@ module.exports.updateUser = async (req, res, next) => {
     }
     res.send(val);
   } catch (error) {
-    if (error.name === 'ValidationError') {
+    if (error.name === "ValidationError") {
       return next(new BadRequest(BAD_REQUEST_VALIDATION_ERROR));
     }
     if (error.code === 11000) {
@@ -59,7 +65,7 @@ module.exports.updateUser = async (req, res, next) => {
 module.exports.login = async (req, res, next) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select("+password");
     if (!user) {
       return next(new Forbidden(WRONG_DATA_RESPONSE));
     }
@@ -67,15 +73,21 @@ module.exports.login = async (req, res, next) => {
     if (!matched) {
       return next(new Forbidden(WRONG_DATA_RESPONSE));
     }
-    const key = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'rabotai', {
-      expiresIn: '7d',
-    });
-    res.cookie('jwt', key, {
-      sameSite: 'None',
-      /*       secure: true, */
-      maxAge: 7777777,
-    /*       httpOnly: true, */
-    }).send({ message: SUCCESS_LOGIN });
+    const key = jwt.sign(
+      { _id: user._id },
+      NODE_ENV === "production" ? JWT_SECRET : "rabotai",
+      {
+        expiresIn: "7d",
+      }
+    );
+    res
+      .cookie("jwt", key, {
+        sameSite: "None",
+        secure: true,
+        maxAge: 7777777,
+        httpOnly: true,
+      })
+      .send({ message: SUCCESS_LOGIN });
   } catch (error) {
     next(error);
   }
@@ -91,5 +103,5 @@ module.exports.me = async (req, res, next) => {
 };
 
 module.exports.signout = async (_, res) => {
-  res.clearCookie('jwt').send({ message: 'Выход' });
+  res.clearCookie("jwt").send({ message: "Выход" });
 };

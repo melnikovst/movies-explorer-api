6f0021c1afae4e898a1c36bc1e876c2ee4140740
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2023-03-07 12:08:21 +0300

    123

--- controllers/users.js
+++ controllers/users.js
@@ -82,7 +82,7 @@ module.exports.login = async (req, res, next) => {
     );
     res
       .cookie("jwt", key, {
-        sameSite: "None",
+        sameSite: "none",
         secure: true,
         maxAge: 7777777,
         httpOnly: true,
